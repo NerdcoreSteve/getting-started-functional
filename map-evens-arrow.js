af67f16baa199cfc2d9d5416da6d15d29a9acf8c
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2016-10-17 10:19:33 -0600

    added arrow functions

---
+++ map-evens-arrow.js
@@ -0,0 +1,3 @@
+const evenValues = arr => arr.filter(x => x % 2 === 0)
+
+console.log(evenValues([1, 2, 3, 4]))
---
+++ map-sum-arrow.js
@@ -0,0 +1,3 @@
+const sumValues = arr => arr.reduce((sum, x) => sum + x)
+
+console.log(sumValues([1, 2, 3, 4]))
